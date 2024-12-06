7a41baaa4fc24075ea4ee37201fb0ec05785f65b
kenny-chua <kenny.chua.edu@outlook.com>
2024-12-06 14:44:04 -0500

    Made some low hanging fruit code changes and left more complex for later

--- app/processor.py
+++ app/processor.py
@@ -4,31 +4,30 @@ from playwright.sync_api import sync_playwright
 import time
 import newspaper
 
+PLAYWRIGHT_TIMEOUT_MILLISECONDS = 60000
+PYSTD_TIME_SECONDS = 3
+
 def clean_malformed_escaped_url(url: str) -> str:
     print(url)
     url = url.replace("\\\\u003d", "=")
     return url
 
 def scrape_with_playwright(url):
-    # Using Playwright to render JavaScript
     with sync_playwright() as p:
-        browser = p.chromium.launch()
-        context = browser.new_context()
-        context.set_default_timeout(60000)
-        page = context.new_page()
-        page.goto(url, wait_until="domcontentloaded", timeout=60000)
-        time.sleep(3) # Allow the javascript to render
-        content = page.content()
-        browser.close()
+        with p.chromium.launch() as browser:
+            context = browser.new_context()
+            context.set_default_timeout(PLAYWRIGHT_TIMEOUT_MILLISECONDS)
+            page = context.new_page()
+            page.goto(url, wait_until="domcontentloaded", timeout=PLAYWRIGHT_TIMEOUT_MILLISECONDS)
+            time.sleep(PYSTD_TIME_SECONDS) # Allow the javascript to render
+            content = page.content()
     article = newspaper.article(url, input_html=content, language='en')
     return article
 
 def get_article_text_and_insert(engine):
 
-    sql_stmt = select(TopHeadline).where(TopHeadline.content.is_(None))
-
     with Session(engine) as session:
-        contents = session.exec(sql_stmt).all()
+        contents = session.exec(select(TopHeadline).where(TopHeadline.content.is_(None))).all()
     
         for content in contents:
             try:
